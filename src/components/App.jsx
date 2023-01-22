e382c749ec1f951f7a1938d2baa009a9dcf8f5db
SerhiiKutovyi <wagenrex@gmail.com>
2023-01-22 02:45:53 +0200

    node_modules

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import styled from 'styled-components';
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <>
+        <Section>
+          <h2>PhoneBook</h2>
+          <p>Name</p>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <button type="button"> Add contact</button>
+        </Section>
+        <section>
+          <h2>Contacts</h2>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </section>
+      </>
+    );
+  }
+}
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin-left: 40px;
+  p {
+    margin-bottom: 20px;
+  }
+  h2 {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 72px;
+
+    color: #000000;
+  }
+
+  input {
+    max-width: 200px;
+
+    margin-bottom: 20px;
+  }
+
+  button {
+    max-width: 100px;
+  }
+`;
