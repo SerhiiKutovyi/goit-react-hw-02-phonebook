0e96b3de801eb45f1977631d3635b2257b8ba0f6
SerhiiKutovyi <wagenrex@gmail.com>
2023-01-23 15:43:51 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,17 @@
+import { nanoid } from 'nanoid';
+
 export const ContactList = ({ contacts }) => {
+  const newId = {
+    id: nanoid(),
+  };
+
   console.log(contacts);
 
   return (
     <>
-      <ul>
+      <ul key={newId}>
         {contacts.map(user => (
-          <li kei={user.id}>{user.name}</li>
+          <li key={newId}>{user.name}</li>
         ))}
       </ul>
     </>
